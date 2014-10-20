8682d073a105a9f0d4f7c968bba5dcf18b45670a
Marks Gniteckis <gniteckm@uni.coventry.ac.uk>
2014-10-20 09:51:11 +0100

    Rearrange

--- currencyconverter.py
+++ currencyconverter.py
@@ -2,16 +2,17 @@ import urllib2
 import json
 
 print "Hello and welcome to Currency Converter v0.1"
-
-pounds = input("amount you wish to exchange: ")
 url = "http://rate-exchange.appspot.com/currency?from=GBP&to=EUR"
-if pounds.isdigit():
-    print total
-else:
-    print "Please, use numeric input, we are not able to convert letters into"
-    "money. (But that would be awesome!)"
-    
 response = urllib2.urlopen(url).read()
 data = json.loads(response.decode('utf8'))
 rates = data ['rate']
+pounds = raw_input("amount you wish to exchange: ")
 total = pounds * rates
+url = "http://rate-exchange.appspot.com/currency?from=GBP&to=EUR"
+
+if pounds.isdigit():
+    print total
+else:
+    print "Please, use numeric input, we are not able to convert letters into money. (But that would be awesome!)"
+
+
